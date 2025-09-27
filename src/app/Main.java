70070d4f85c6619d99d34540afdd8ad70871803b
Volodymyr Kirillov <volodymyrkirillov@gmail.com>
2025-09-27 15:24:41 +0300

    My third commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,25 @@
 package app;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Shop-online for customers");
+    static void main() {
+        Person john = new Person("John", 30, "Engineer");
+        Person bob = new Person("Bob", 35, "Doctor");
+        Person mary = new Person("Mary", 25, "Teacher");
+
+
+        System.out.println(john);
+        System.out.println(bob);
+
+        System.out.println("Before profession update");
+        System.out.println(mary);
+
+        boolean result = mary.setProfession("Architecture");
+        if(!result) System.out.println("Invalid Profession");
+
+        System.out.println("After profession update");
+
+        System.out.println(mary);
+
+
     }
 }
---
+++ src/app/Person.java
@@ -0,0 +1,56 @@
+package app;
+
+public class Person {
+    private String name;
+    private int age;
+    private String profession;
+    private final int id;
+    private static int personCount;
+
+
+
+    public Person( String name, int age, String profession ) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+        this.id = personCount++;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getProfession() {
+        return this.profession;
+    }
+
+    public boolean setAge(int age) {
+        if(age <= 0 || age > 100) return false;
+
+        this.age = age;
+        return true;
+    }
+
+    public boolean setProfession(String profession) {
+        if(profession.isBlank()) return false;
+
+        this.profession = profession;
+        return true;
+    }
+
+    public boolean setName(String name) {
+        if(name.isBlank()) return false;
+
+        this.name = name;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + ". Name: " + this.name + ", Age: " + this.age + ", Profession: " + this.profession;
+    }
+}
