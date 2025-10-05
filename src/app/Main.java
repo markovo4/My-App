738ef625c589d9f723347ebc62616e75ce356e95
Volodymyr Kirillov <volodymyrkirillov@gmail.com>
2025-10-05 12:23:36 +0300

    HW-5.1

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,29 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+    static String taxTemplate = "Your total tax based on %f%% tax rate: $%f";
+
+    public static void calculateTax ( long annualSalary, float taxPercent ) {
+        double calculatedTax = ( taxPercent / 100 ) * annualSalary;
+        System.out.printf(taxTemplate, taxPercent, calculatedTax);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Shop-online for customers");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter your annual salary for tax calculation: ");
+
+        long annualSalary = sc.nextLong();
+        if(annualSalary < 0) System.out.println("Invalid salary!");
+
+        if(annualSalary <= 10_000){
+            calculateTax(annualSalary, 2.5f);
+        } else if (annualSalary <= 25_000) {
+            calculateTax(annualSalary, 4.3f);
+        } else {
+            calculateTax(annualSalary, 6.7f);
+        }
+
     }
 }
